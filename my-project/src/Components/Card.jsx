ed5cbec68d2dcab23234bbbbf6452b993f78ed32
AnkitChauhan39 <ankitchauhanbly88@gmail.com>
2024-10-04 19:13:48 +0530

    projects section added

--- my-project/src/Components/Card.jsx
+++ my-project/src/Components/Card.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { FaGithub } from "react-icons/fa";
 
 const Card = ({project}) => {
 
 
   return (
-    <div className='p-4' >
-        <div className=' border-2 w-full h-[300px] '  ></div>
+    <div className='p-4 bg-[#2e5ee212] ' >
+        <div className=' shadow-2xl w-full h-[400px] rounded-2xl overflow-hidden '>
+            <img className='h-full w-full object-cover ' src={project.page}></img>
+        </div>
+        <div className='p-4' >
+            <div className='text-white text-[32px] font-semibold ' >{project.name}</div>
+            <div className='text-[#cecbcbac]' >{project.decription}</div>
+        </div>
+        <div className='p-4 flex gap-10  items-center'>
+            <button className='p-4 shadow-2xl rounded-2xl bg-red-900 text-white w-[150px] font-semibold text-[18px] ' > LIVE LINK </button>
+            <FaGithub className='text-[50px] text-white ' />
+        </div>
     </div>
   )
 }
--- my-project/src/Components/Project.jsx
+++ my-project/src/Components/Project.jsx
@@ -3,11 +3,17 @@ import dev from '../assets/devImage.png'
 import ecoSathi from '../assets/Screenshot 2024-10-04 150408.png'
 import sphere from '../assets/SphereImage.png'
 import Card from './Card'
+import devpage  from '../assets/DevAuctionPage.png'
+import Ecopage  from '../assets/ecoSathifirst.png'
+import cryptopage  from '../assets/Crypto.png'
+import Crypto from '../assets/Cryptoimg.png'
+import spherepage from '../assets/spherepage.png'
 
 const Project = () => {
 
   const [visible,setvisible] = useState({
     name : "DEV-AUCTION",
+    page: devpage,
     image: dev , 
     decription :"A Full stack web application build to connect Potential buyers with the authentic developer" , 
     tags : ["ReactJS","NodeJs","Express","MongoDB","Render"] 
@@ -16,22 +22,32 @@ const Project = () => {
   const projects = [
     {
       name : "DEV-AUCTION",
+      page: devpage,
       image: dev , 
       decription :"A full-stack project that connects developers with buyers, ensuring fair compensation for innovative projects. DevAuction enables developers to auction their projects, ensuring fair compensation. Buyers place bids with the highest bidder receiving the source code" , 
       tags : ["ReactJS","NodeJs","Express","MongoDB","Render"]  
     },
     {
       name : "Eco-Sathi",
+      page: Ecopage,
       image: ecoSathi , 
       decription :"EcoSathi is an innovative platform designed to bridge the gap between local ragpickers and users, making it easy to schedule waste collection services. Our mission is to create a seamless and efficient system that benefits both the community and the environment." , 
       tags : ["ReactJS","NodeJS","Express","Google-maps-API"] , 
     },
     {
       name : "Arithimatic sphere Alogrithm",
+      page: spherepage,
       image: sphere, 
       decription :"Analyzing arithmetic sphere algorithm with Python libraries and 3D visualization tools, prioritizing accuracy and performance. The analysis focuses on applications in sphere generation, computer graphics, virtual reality, image processing, mathematical modeling, and data visualization." , 
       tags : ["Python","numpy","matplotlib"] , 
-    }
+    },
+    {
+      name : "Crypto-Hunter",
+      page: cryptopage,
+      image: Crypto , 
+      decription :" Developed the application from scratch, implementing features to track and display real-time crypto currency prices and market trends . Using Chart js Prepared a Historical line chart displaying the price of the coin in past 1 , 30 and 365 days fetched from historical data API" , 
+      tags : ["ReactJS","Material-UI","Chart-js"] , 
+    },
   ]
 
   return (
@@ -45,17 +61,36 @@ const Project = () => {
       </div>
       <div className='mt-[50px]' >
         <div className='flex justify-around ' >
-          <div className='rounded-full h-[125px] w-[125px] overflow-hidden ' >
+          <div className='rounded-full h-[125px] w-[125px] overflow-hidden ' 
+              onClick={ () =>{
+                setvisible(projects[0]) 
+              } }
+          >
             <img className='h-full w-full object-cover rounded-full' src={dev}></img>
           </div>
-          <div className='rounded-full h-[125px] w-[125px] overflow-hidden ' >
+          <div className='rounded-full h-[125px] w-[125px] overflow-hidden '
+              onClick={ () =>{
+                setvisible(projects[1]) 
+              } } 
+          >
             <img className='h-full w-full object-cover rounded-full' src={ecoSathi}></img>
           </div>
-          <div className='rounded-full h-[125px] w-[125px] overflow-hidden ' >
+          <div className='rounded-full h-[125px] w-[125px] overflow-hidden '
+              onClick={ () =>{
+                setvisible(projects[2]) 
+              } }
+           >
             <img className='h-full w-full object-cover rounded-full' src={sphere}></img>
           </div>
+          <div className='rounded-full h-[125px] w-[125px] overflow-hidden ' 
+              onClick={ () =>{
+                setvisible(projects[3]) 
+              } }
+          >
+            <img className='h-full w-full object-cover rounded-full' src={Crypto}></img>
+          </div>
         </div>
-        <div className='border-4 mt-[50px] h-[400px] ' >
+        <div className='shadow-2xl rounded-2xl border-slate-500 mt-[50px]  ' >
           <Card project={visible}/>
         </div>
       </div>
